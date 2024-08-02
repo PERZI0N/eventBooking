3802988beeca409974ffba97ce33daa7c55fb640
PERZI0N <vaidik.chhirolya1@gmail.com>
2024-08-02 16:30:49 +0530

    changes

--- serverSide/src/app.ts
+++ serverSide/src/app.ts
@@ -9,7 +9,13 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(
+  cors({
+    origin: ["https://event-booking-nap-queens-frontend.vercel.app/"],
+    methods: ["POST", "GET", "DELETE"],
+    credentials: true
+  })
+);
 mongoose
   .connect(`${process.env.DB_URL}`)
   .then(() => console.log("MongoDB connected successfully."))
